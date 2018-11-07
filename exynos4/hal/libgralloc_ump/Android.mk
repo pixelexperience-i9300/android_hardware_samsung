d2ec13c4b2f91d534d26a0e74e94eebc758b7956
Shilin Victor <chrono.monochrome@gmail.com>
2018-11-07 15:52:01 +0300

    Revert "exynos4: android.hardware.graphics.allocator@2.0 for Exynos4"
    This reverts commit 3134d2eea5bc6b2a29de2faec067aea5e5d0a44e.
    
    Change-Id: I2f80b912bbecfd2b72e668c2db0701c3e7008aa1

--- exynos4/hal/libgralloc_ump/Android.mk
+++ exynos4/hal/libgralloc_ump/Android.mk
@@ -49,7 +49,7 @@ LOCAL_CFLAGS += -Wno-error
 LOCAL_CFLAGS += -DSAMSUNG_EXYNOS
 LOCAL_CFLAGS += -DSAMSUNG_EXYNOS_CACHE_UMP
 
-LOCAL_STATIC_LIBRARIES        := libgralloc1-adapter-exynos4
+LOCAL_STATIC_LIBRARIES        := libgralloc1-adapter
 LOCAL_SHARED_LIBRARIES        += libsync
 
 ifeq ($(TARGET_SOC),exynos4210)
