a416ce337844399659e05886c978a0e7a4f2e106
Shilin Victor <chrono.monochrome@gmail.com>
2019-01-27 00:31:09 +0300

    Revert "hwc: verbose layer debugging"
    This reverts commit e0674a62cd6d817b15920d7df9ef6a05dd6ffa7b.
    
    Change-Id: I9bb6ab964f2dba3d68a34c9737a7a56401e79b1f

--- exynos4/hal/libhwc/hwcomposer.cpp
+++ exynos4/hal/libhwc/hwcomposer.cpp
@@ -292,23 +292,14 @@ bool is_overlay_supported(struct hwc_context_t *ctx, hwc_layer_1_t &layer, size_
         if (!supports_fimg(layer)) {
             ALOGW("\tlayer %u: FIMG required but not supported", i);
             return false;
-        } else {
-            ALOGW("\t<fimg> layer %u, format %u, cType=%d, hints=%d, flags=%d, w=%d, h=%d, tr=%d, bl=%d, a=%d", i, handle->format, layer.compositionType,
-				layer.hints, layer.flags, WIDTH(layer.displayFrame), HEIGHT(layer.displayFrame), layer.transform, layer.blending, layer.planeAlpha);
-
-	}
+        }
         break;
 #endif
     case gsc_map_t::FIMC:
         if (!supports_fimc(layer)) {
-            ALOGW("\tlayer %u: FIMC required but not supported", i);
+            ALOGW("\tlayer %u: FIMG required but not supported", i);
             return false;
-        } else {
-            ALOGW("\t<fimg> layer %u, format %u, cType=%d, hints=%d, flags=%d, w=%d, h=%d, tr=%d, bl=%d, a=%d", i, handle->format, layer.compositionType,
-				layer.hints, layer.flags, WIDTH(layer.displayFrame), HEIGHT(layer.displayFrame), layer.transform, layer.blending, layer.planeAlpha);
-
-	}
-
+        }
         break;
 
     default:
@@ -316,11 +307,7 @@ bool is_overlay_supported(struct hwc_context_t *ctx, hwc_layer_1_t &layer, size_
 				|| !is_x_aligned(layer)) {
             ALOGW("\tlayer %u: pixel format %u not supported", i, handle->format);
             return false;
-        } else {
-            ALOGW("\t<fimg> layer %u, format %u, cType=%d, hints=%d, flags=%d, w=%d, h=%d, tr=%d, bl=%d, a=%d", i, handle->format, layer.compositionType,
-				layer.hints, layer.flags, WIDTH(layer.displayFrame), HEIGHT(layer.displayFrame), layer.transform, layer.blending, layer.planeAlpha);
-
-	}
+        }
     }
 
     if (visible_width(ctx, layer) < BURSTLEN_BYTES) {
