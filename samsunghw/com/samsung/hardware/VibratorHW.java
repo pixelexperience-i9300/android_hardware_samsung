a94108c03e5fe8c5fcb20af37d521a3fd0cedc94
Danilo Santos <dansantosbe@gmail.com>
2019-03-07 18:25:04 -0300

    samsung: hardware: import dotos FileUtils

--- samsunghw/com/samsung/hardware/VibratorHW.java
+++ samsunghw/com/samsung/hardware/VibratorHW.java
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-package org.lineageos.hardware;
+package com.samsung.hardware;
 
-import org.lineageos.internal.util.FileUtils;
+import com.android.internal.util.dotos.FileUtils;
 
 public class VibratorHW {
 
